fb27560fba56e79f921e26aac902279fff6ee71b
Erik Rintala <gerfiglmac@gmail.com>
2017-07-18 21:49:58 -0700

    Test main

--- main.c
+++ main.c
@@ -2,9 +2,25 @@
 
 int main(int argc, char **argv)
 {
-	setup_echo();
-	read_echo_pos(getmaxy(stdscr) - 2, 10);
-	usleep(DURATION * 10);
-	exit_screen();
-	return 1;
+	setup();
+	char *str = argv[1];
+	char *cpy = ft_strdup(str);
+	while (1)
+	{
+		for (int j = 0; j < getmaxy(stdscr); j+=2)
+		{
+			for (int i = 0; i < 50; i++)
+			{
+				print_text(j, 0, str);
+				print_text(j + 1, 0, cpy);
+			}
+		}
+		str = scroll_left(str);
+		cpy = scroll_right(cpy);
+		refresh();
+		usleep(DURATION);
+	}
+	usleep(DURATION * 50);
+ 	endwin();
+	return 0;
 }
