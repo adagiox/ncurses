04d6293f1154649341849ff84bbdd74c6db10b00
Erik Rintala <gerfiglmac@gmail.com>
2017-07-19 16:07:37 -0700

    Some text echoing functions

--- main.c
+++ main.c
@@ -2,25 +2,12 @@
 
 int main(int argc, char **argv)
 {
-	setup();
+	setup_echo();
+	move(0, 0);
 	char *str = argv[1];
-	char *cpy = ft_strdup(str);
-	while (1)
-	{
-		for (int j = 0; j < getmaxy(stdscr); j+=2)
-		{
-			for (int i = 0; i < 50; i++)
-			{
-				print_text(j, 0, str);
-				print_text(j + 1, 0, cpy);
-			}
-		}
-		str = scroll_left(str);
-		cpy = scroll_right(cpy);
-		refresh();
-		usleep(DURATION);
-	}
-	usleep(DURATION * 50);
+	clear();
+	read_echo_pos(getmaxy(stdscr) - 1, 0);
+	usleep(DURATION);
  	endwin();
 	return 0;
 }
--- textinput.c
+++ textinput.c
@@ -1,10 +1,37 @@
 #include "draw.h"
 
-int read_echo_pos(int y, int x)
+int read_print_pos(int y, int x)
 {
 	char *str = ft_strnew(getmaxx(stdscr));
 	move(getmaxy(stdscr), 0);
 	getnstr(str, getmaxx(stdscr));
 	mvprintw(y, x, str);
+	return 1;
+}
+
+// Function to move back to previous cursor location
+
+// Function to echo at a position
+
+// Function to delete character and move cursor back
+
+int read_echo_pos(int y, int x)
+{
+	char ch;
+	int prow, pcol;
+	int row = y;
+	int col = x;
+	getyx(stdscr, prow, pcol);
+	while ((ch = getch()) != 10)
+	{
+		if (ch == 8 && col > 0)
+		{
+			delch();
+			col--;
+		}
+		mvaddch(row, col, ch);
+		move(prow, pcol);
+		refresh();
+	}
 	return 1;
 }
